788629398eb8e4109c29d8d13ae8e4bcf0435c35
anirudh-atodaria-interlincx <anirudh.atodaria@lincx.la>
2023-02-01 01:42:55 +0530

    route rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+      },
+      {
+        source: "/:customPage",
+        destination: "/:customPage/index.html",
+      },
+      {
+        source: "/:customPage",
+        destination: "/:customPage.html"
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
